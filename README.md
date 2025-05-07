f940e6019476b3ce3ac3454f2f3f8c279a0c7211
Kayla-Phillips <k3phillips@ucsd.edu>
2025-05-06 19:14:26 -0700

    readme questions and unit tests

--- README.md
+++ README.md
@@ -3,4 +3,6 @@ Make sure you make a PR to your own repo's main and not the class' repo!! Otherw
 
 Kayla Phillips
 
-1. 
+1. No because the "message" features involves the interaction between many different components across the app, making it end-to-end. These types of features are not ideal for unit testing.
+   
+2. Yes, because the "max message length" feature would typically be a small, isolated component, which is ideal for unit testing. 
--- __tests__/unit.test.js
+++ __tests__/unit.test.js
@@ -9,3 +9,89 @@ import {
 } from '../code-to-unit-test/unit-test-me';
 
 // TODO - Part 2
+
+// isPhoneNumber Tests
+test('123-456-7890 is a valid phone number', () => {
+  expect(isPhoneNumber("123-456-7890")).toBe(true);
+});
+
+test('(123) 456-7890 is a valid phone number', () => {
+  expect(isPhoneNumber("(123) 456-7890")).toBe(true);
+});
+
+test('1234567890 is not a valid phone number', () => {
+  expect(isPhoneNumber("1234567890")).toBe(false);
+});
+
+test('abc-def-ghik is not a valid phone number', () => {
+  expect(isPhoneNumber("abc-def-ghij")).toBe(false);
+});
+
+// isEmail Tests
+test('user@example.com is a valid email', () => {
+  expect(isEmail("user@example.com")).toBe(true);
+});
+
+test('name123@example.net is a valid email', () => {
+  expect(isEmail("name123@example.net")).toBe(true);
+});
+
+test('userexample.com is not a valid email', () => {
+  expect(isEmail("userexample.com")).toBe(false);
+});
+
+test('user@domain.corporate is not a valid email', () => {
+  expect(isEmail("user@domain.corporate")).toBe(false);
+});
+
+// isStrongPassword Tests
+test('abc1 is a strong password', () => {
+  expect(isStrongPassword("abc1")).toBe(true);
+});
+
+test('K_password2 is a strong password', () => {
+  expect(isStrongPassword("K_password2")).toBe(true);
+});
+
+test('123abc is not a strong password', () => {
+  expect(isStrongPassword("123abc")).toBe(false);
+});
+
+test('x is not a strong password', () => {
+  expect(isStrongPassword("x")).toBe(false);
+});
+
+// isDate Tests
+test('1/1/2025 is a valid date', () => {
+  expect(isDate("1/1/2025")).toBe(true);
+});
+
+test('12/25/2025 is a valid date', () => {
+  expect(isDate("12/25/2025")).toBe(true);
+});
+
+test('1/1/25 is not a valid date', () => {
+  expect(isDate("1/1/25")).toBe(false);
+});
+
+test('1-1-2025 is not a valid date', () => {
+  expect(isDate("1-1-2025")).toBe(false);
+});
+
+// isHexColor Tests
+test('#FFF is a valid hex color', () => {
+  expect(isHexColor("#FFF")).toBe(true);
+});
+
+test('#abc123 is a valid hex color', () => {
+  expect(isHexColor("#abc123")).toBe(true);
+});
+
+test('#FFFF is not a valid hex color', () => {
+  expect(isHexColor("#FFFF")).toBe(false);
+});
+
+test('#xyz is not a valid hex color', () => {
+  expect(isHexColor("#xyz")).toBe(false);
+});
+

b9052e045473764d78fe4d157153b4b0c714fb20
Kayla-Phillips <k3phillips@ucsd.edu>
2025-05-06 18:23:41 -0700

    finished speech synthesis

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -4,4 +4,47 @@ window.addEventListener('DOMContentLoaded', init);
 
 function init() {
   // TODO
+  const synth = window.speechSynthesis;
+  const voiceSelect = document.getElementById("voice-select");
+  const speakButton = document.querySelector("button");
+  const textInput = document.getElementById("text-to-speak");
+  const faceImage = document.querySelector("img");
+
+  let voices = [];
+
+  function populateVoiceList(){
+    voices = synth.getVoices();
+    voiceSelect.innerHTML = '<option value="select" disabled selected>Select Voices:</option>';
+
+    voices.forEach((voice, index) => {
+      const option = document.createElement("option");
+      option.textContent = `${voice.name}`;
+      option.value = index;
+      voiceSelect.appendChild(option);
+    });
+  }
+
+  synth.addEventListener("voiceschanged", populateVoiceList);
+  populateVoiceList();
+
+  speakButton.addEventListener("click", function() {
+    const text = textInput.value;
+    const selectedIndex = voiceSelect.value;
+
+    if (!text || selectedIndex === "select") {
+      alert("Please enter text and select a voice.");
+      return;
+    }
+
+    const utterance = new SpeechSynthesisUtterance(text);
+    utterance.voice = voices[selectedIndex];
+
+    faceImage.src = "assets/images/smiling-open.png";
+
+    utterance.addEventListener("end", function(){
+      faceImage.src = "assets/images/smiling.png";
+    });
+
+    synth.speak(utterance);
+  })
 }
